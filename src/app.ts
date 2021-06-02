03022e8e5317aaf4280670b58f61af5d3fcc82ff
tsjrw <tesexbox360@hotmail.com>
2021-06-02 18:14:39 +0100

    Feature: added morgan package

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,10 @@
-import 'dotenv/config'
+import 'dotenv/config';
 import express from 'express';
+import morgan from 'morgan';
 
 const app = express();
+app.use(morgan('dev'));
 
 app.listen(process.env.PORT, () => {
-  return console.log(`server is listening on ${process.env.PORT}`);
+    return console.log(`server is listening on ${process.env.PORT}`);
 });
