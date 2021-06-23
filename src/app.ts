b2eb331d396f85d2d46680930d2c06934883002a
tsjrw <tesexbox360@hotmail.com>
2021-06-23 14:38:05 +0100

    FEATURE: Error handling

--- src/app.ts
+++ src/app.ts
@@ -1,20 +1,22 @@
 import 'dotenv/config';
-import express from 'express';
+import express, { NextFunction, Request, Response } from 'express';
 import morgan from 'morgan';
 
-import routes from './routes'
+import routes from './routes';
+import { errorHandler } from './errorHandler';
 
 const app = express();
 app.use(morgan('dev'));
-app.use(express.json())
-app.use(express.urlencoded({extended: true}))
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.use('/users', routes.users)
-app.use('/customers', routes.customers)
+app.use('/users', routes.users);
+app.use('/customers', routes.customers);
 
-app.listen(process.env.PORT, () => {
-    return console.log(`server is listening on ${process.env.PORT}`);
+app.use((err: any, req: Request, res: Response, next: NextFunction) => {
+  return errorHandler(err, req, res);
 });
 
-
-
+app.listen(process.env.PORT, () => {
+  return console.log(`server is listening on ${process.env.PORT}`);
+});
