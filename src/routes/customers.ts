375565c94d5d0e8132462424f7c7cc0a577ea385
tsjrw <tesexbox360@hotmail.com>
2021-07-28 12:44:23 +0100

    FEATURE: Users auth added to customer routes

--- src/routes/customers.ts
+++ src/routes/customers.ts
@@ -1,14 +1,15 @@
 import { Router } from 'express';
+import { verifyToken } from '../auth';
 import controllers from '../controllers';
 
 const router = Router();
 
 const { list, create, customerInfo, update, remove } = controllers.customers;
 
-router.get('/', list);
-router.get('/:id', customerInfo);
-router.post('/', create);
-router.put('/:id', update)
-router.delete('/:id', remove)
+router.get('/', verifyToken, list);
+router.get('/:id', verifyToken, customerInfo);
+router.post('/', verifyToken, create);
+router.put('/:id', verifyToken, update);
+router.delete('/:id', verifyToken, remove);
 
 export default router;
